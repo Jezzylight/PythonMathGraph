cec30a52032e16501f3117e522e5ddabe0123159
Jezzylight <37625125+Jezzylight@users.noreply.github.com>
2018-03-22 02:17:39 +0700

    READYFUNC0

---
+++ func.py
@@ -0,0 +1,184 @@
+#%matplotlib inline
+
+func = '1 s-similar'
+a=int(input())
+b=int(input())
+x=int(input())
+
+def s_similar(x,a,b):
+    if x <= a:
+        return 0
+    elif a <= x <= ((a + b) / 2):
+        return (2 * ((x - a) ** 2)) / ((b - a) ** 2))
+    elif (a + b) / 2 <= x <= b:
+        return 1 - (2 * ((x - a) ** 2)) / ((b - a) ** 2))
+    else:
+        return 1
+
+
+
+
+func = '2 pi-similar'
+
+a=int(input())
+b=int(input())
+c=int(input())
+x=int(input()) 
+
+def pi_similar(x,a,b,c):
+    if x < b:
+        return s_similar(x, a, b)
+    if b <= x <= c:
+        return 1
+    if x > c:
+        return 1 - s_similar(x, c, c + b - a)
+
+       
+       
+ func = '3 triangle'
+
+a=int(input())
+b=int(input())
+c=int(input())
+x=int(input()) 
+
+def triangle(x,a,b,c):
+    if x <= a:
+        return 0
+    if a < x <= c:
+        return (x - c) / (c - a)
+    if c < x <= b:
+        return (b - x) / (b - c)
+    if x >= b:
+        return 0
+
+
+func = '4 trapeze '
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+x=int(input()) 
+
+def trapeze(x,a,b,c,d):
+    if x <= a:
+        return 0
+    if a < x < c:
+        return (x - a) / (c - a)
+    if c <= x <= d:
+        return 1
+    if d < x < b:
+        return (b - x) / (b - d)
+    if x >= b:
+        return 0
+
+
+
+
+func = '5 sigm'
+import math
+
+
+a=int(input())
+b=int(input())
+x=int(input())
+
+def sigm(x,a,b):
+    return 1 / (1 + math.exp(-a * (x - b)))
+
+
+func = '6 z-similar'
+
+a=int(input())
+b=int(input())
+c=int(input())
+x=int(input()) 
+
+def z_similar(x,a,b,c):
+    if x <= c:
+        return 1
+    elif x > c:
+        return 1/(1+(a*(x-c))**b)
+   
+
+
+func = '7 Gauss'
+import math
+
+
+a=int(input())
+b=int(input())
+x=int(input()) 
+
+def Gauss(x,a,b):
+    return math.exp((-1) * (((x - a) ** 2) / (2*(b ** 2))))
+
+
+
+
+func = 'bell-shaped 8'
+import math
+
+a=int(input())
+b=int(input())
+x=int(input())
+
+def bell_shaped(a,b,x):
+    return 1 / (1 + math.fabs(((x - c) / a) ** (2 * b))
+
+
+
+func = '9 diff-sigm'
+import math
+
+a1=int(input())
+b1=int(input())
+a2=int(input())
+b2=int(input())
+x=int(input())
+
+def diff-sigm(x,a1,b1,a2,b2):
+    return (1 / (1 + math.exp(-a1 * (x - b1))))-(1 / (1 + math.exp(-a2 * (x - b2))))
+
+
+
+func = '10 bilatetal-Gauss'
+import math
+
+a1=int(input())
+c1=int(input())
+a2=int(input())
+c2=int(input())
+x=int(input())
+
+if c1<c2:
+    def bilatetal-Gauss(x,a1,c1,a2,c2):
+     if x < c1:
+        return math.exp((x-c1)**2)/((-2*a1)**2)
+    elif c1 <= x <= c2:
+        return 1
+    elif if x > c2:
+        return math.exp((x-c2)**2)/((-2*a2)**2)
+elif c1>c2:
+    def bilatetal-Gauss(x,a1,c1,a2,c2):
+     if x < c2:
+        return math.exp((x-c1)**2)/((-2*a1)**2)
+    elif c2 <= x <= c1:
+        return (math.exp((x-c1)**2)/((-2*a1)**2))*(math.exp((x-c2)**2)/((-2*a2)**2))
+    elif if x > c1:
+        return math.exp((x-c2)**2)/((-2*a2)**2)
+
+
+
+func = '11 comp-sigm'
+import math
+
+a1=int(input())
+b1=int(input())
+a2=int(input())
+b2=int(input())
+x=int(input())
+
+def comp-sigm(x,a1,b1,a2,b2):
+    return (1 / (1 + math.exp(-a1 * (x - b1))))*(1 / (1 + math.exp(-a2 * (x - b2))))
